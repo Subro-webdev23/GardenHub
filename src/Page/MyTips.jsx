9073c7306bab651b632f15c921826cfd0a152135
Subro-webdev23 <subrota.webdev23@gmail.com>
2025-05-23 11:25:53 +0600

    delete method added

--- src/Page/MyTips.jsx
+++ src/Page/MyTips.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
-import { Link, useLoaderData } from 'react-router';
+import React, { useState } from 'react';
+import { useLoaderData } from 'react-router';
+import TipsTable from './TipsTable';
 
 const MyTips = () => {
     const data = useLoaderData();
+    const [tips, setTips] = useState(data)
     console.log(data);
+
 
     return (
         <div className="max-w-6xl mx-auto p-6">
@@ -23,39 +26,7 @@ const MyTips = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {data.map((tip) => (
-                            <tr key={tip._id} className="border-t hover:bg-gray-50">
-                                <td className="px-6 py-4">
-                                    <img
-                                        src={tip.imagesURL}
-                                        alt={tip.title}
-                                        className="h-12 w-12 rounded object-cover"
-                                    />
-                                </td>
-                                <td className="px-6 py-4 font-medium text-gray-900">{tip.title}</td>
-                                <td className="px-6 py-4">{tip.topic}</td>
-                                <td className="px-6 py-4">{tip.category}</td>
-                                <td className="px-6 py-4">{tip.level}</td>
-                                <td className="px-6 py-4 capitalize">
-                                    <span
-                                        className={`px-2 py-1 rounded text-xs font-medium ${tip.availability === 'public'
-                                            ? 'bg-green-100 text-green-700'
-                                            : 'bg-yellow-100 text-yellow-700'
-                                            }`}
-                                    >
-                                        {tip.availability}
-                                    </span>
-                                </td>
-                                <td className="px-6 py-4 flex justify-center gap-3">
-                                    <Link to={`/update/${tip._id}`} className="px-3 py-1 text-sm text-white bg-blue-600 rounded hover:bg-blue-700 transition">
-                                        Update
-                                    </Link>
-                                    <button className="px-3 py-1 text-sm text-white bg-red-600 rounded hover:bg-red-700 transition">
-                                        Delete
-                                    </button>
-                                </td>
-                            </tr>
-                        ))}
+                        {tips.map((tip) => <TipsTable tips={tips} setTips={setTips} tip={tip} key={tip._id}></TipsTable>)}
                     </tbody>
                 </table>
             </div>
---
+++ src/Page/TipsTable.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { Link } from 'react-router';
+import Swal from 'sweetalert2';
+
+const TipsTable = ({ tip, setTips, tips }) => {
+    const handleDelete = (_id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            console.log(result.isConfirmed)
+            if (result.isConfirmed) {
+
+                // start deleting the Tip
+                fetch(`http://localhost:3000/update/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your Tip has been deleted.",
+                                icon: "success"
+                            });
+
+                            // remove the Tips from the state
+                            const remainingTips = tips.filter(tip => tip._id !== _id);
+                            setTips(remainingTips);
+                        }
+                    })
+
+
+            }
+        });
+    }
+    return (
+        <tr className="border-t hover:bg-gray-50">
+            <td className="px-6 py-4">
+                <img
+                    src={tip.imagesURL}
+                    alt={tip.title}
+                    className="h-12 w-12 rounded object-cover"
+                />
+            </td>
+            <td className="px-6 py-4 font-medium text-gray-900">{tip.title}</td>
+            <td className="px-6 py-4">{tip.topic}</td>
+            <td className="px-6 py-4">{tip.category}</td>
+            <td className="px-6 py-4">{tip.level}</td>
+            <td className="px-6 py-4 capitalize">
+                <span
+                    className={`px-2 py-1 rounded text-xs font-medium ${tip.availability === 'public'
+                        ? 'bg-green-100 text-green-700'
+                        : 'bg-yellow-100 text-yellow-700'
+                        }`}
+                >
+                    {tip.availability}
+                </span>
+            </td>
+            <td className="px-6 py-4 flex justify-center gap-3">
+                <Link to={`/update/${tip._id}`} className="px-3 py-1 text-sm text-white bg-blue-600 rounded hover:bg-blue-700 transition">
+                    Update
+                </Link>
+                <Link onClick={() => handleDelete(tip._id)} className="px-3 py-1 text-sm text-white bg-red-600 rounded hover:bg-red-700 transition">
+                    Delete
+                </Link>
+            </td>
+        </tr>
+    );
+};
+
+export default TipsTable;
